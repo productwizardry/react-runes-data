4b8609093919c2adad8c784e81edc75410f5e304
Merlin Patterson <merlinpatt@gmail.com>
2018-07-14 02:04:49 -0400

    Switch to module build of firebase

--- database/firebase.js
+++ database/firebase.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import firebase from 'firebase';
+import firebase from '@firebase/app';
+import '@firebase/database';
 
 const configurationUrl = 'https://merlinpatt.com/slides/react/scripts/configurationData.json';
 
